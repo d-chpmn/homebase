f8d6192c0a442c7c954dea9e99494fb00a28599f
d-chpmn <d-chpmn@users.noreply.github.com>
2025-11-12 23:44:46 -0600

    chore(migrate): rename JS to JSX files for Vite compatibility and fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import ToDo from "./pages/ToDo";
-import Quotes from "./pages/Quotes";
-import Messages from "./pages/Messages";
-import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard.jsx";
+import ToDo from "./pages/ToDo.jsx";
+import Quotes from "./pages/Quotes.jsx";
+import Messages from "./pages/Messages.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
 
